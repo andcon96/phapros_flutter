7bbab746738f2f303defb1e551ed6eaea9131754
Michael-IMI <michael@ptimi.co.id>
2023-09-22 17:38:34 +0700

    push prod

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'PO Receipt PROD',
+      title: '[PROD]PO Receipt',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
