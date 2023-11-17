72cd7025f7dede0a016b691c71c21f2351c709b9
Michael-IMI <michael@ptimi.co.id>
2023-11-17 10:24:07 +0700

    change environment to live

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: '[PROD]PO Receipt',
+      title: '[SIM]PO Receipt',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/utils/globalurl.dart
+++ lib/utils/globalurl.dart
@@ -5,12 +5,16 @@ library my_prj.globals;
 // String globalurlphoto = 'http://192.168.0.124/e-recmatsim';
 
 // ip local phapros live
-String globalurl = 'http://192.168.0.124/e-recmat/api';
-String globalurlphoto = 'http://192.168.0.124/e-recmat';
+// String globalurl = 'http://192.168.0.124/e-recmat/api';
+// String globalurlphoto = 'http://192.168.0.124/e-recmat';
 
 // domain public
 //  String globalurl = 'https://kacaerp.phapros.co.id/e-recmat/api';
 //  String globalurlphoto = 'https://kacaerp.phapros.co.id/e-recmat';
+
+// domain public sim
+ String globalurl = 'https://kacaerp.phapros.co.id/e-recmat/api';
+ String globalurlphoto = 'https://kacaerp.phapros.co.id/e-recmat';
 
 // ip local
 // String globalurl = 'http://30.30.30.77:8000/api';
