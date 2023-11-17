853e7813a6409f149be38ec38a9825e734adae39
andcon96 <andrew.conan1996@gmail.com>
2023-11-17 10:09:01 +0700

    SetState & Nama Dev

--- lib/utils/globalurl.dart
+++ lib/utils/globalurl.dart
@@ -1,12 +1,12 @@
 library my_prj.globals;
 
 // ip local phapros sim
-// String globalurl = 'http://192.168.0.124/e-recmatsim/api';
-// String globalurlphoto = 'http://192.168.0.124/e-recmatsim';
+String globalurl = 'http://192.168.0.124/e-recmatsim/api';
+String globalurlphoto = 'http://192.168.0.124/e-recmatsim';
 
 // ip local phapros live
-String globalurl = 'http://192.168.0.124/e-recmat/api';
-String globalurlphoto = 'http://192.168.0.124/e-recmat';
+// String globalurl = 'http://192.168.0.124/e-recmat/api';
+// String globalurlphoto = 'http://192.168.0.124/e-recmat';
 
 // domain public
 //  String globalurl = 'https://kacaerp.phapros.co.id/e-recmat/api';
